bf59554dc36ce7f52c5ecf35a8727a01e5e1a552
musudik <kirun.zoom@gmail.com>
2026-04-16 23:45:46 +0200

    Remove GitHub Pages deployment workflow and 404 handling; update base path in Vite config

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,6 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App'
 
-// Handle GitHub Pages 404 redirect
-const redirect = new URLSearchParams(window.location.search).get('redirect')
-if (redirect) {
-  window.history.replaceState(null, '', redirect)
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
